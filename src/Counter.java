5eb9fcd596db0eb77bf76c769114e7d4e992fca3
allenmopom <allen.mopom@gmail.com>
2015-04-30 21:55:04 +0400

    First Commit

---
+++ src/Counter.java
@@ -0,0 +1,13 @@
+/**
+ * Created by mopom on 4/30/15.
+ */
+public class Counter {
+
+    protected long count = 0;
+
+    public void add(long value){
+        this.count = this.count + value;
+    }
+
+
+}
